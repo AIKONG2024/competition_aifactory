469bdbec7bb85526ccf1a1f1f9eed003c80e9594
AIKONG2024 <aikong2024@gmail.com>
2024-03-10 16:07:03 +0900

    -add def

--- train.py
+++ train.py
@@ -11,12 +11,12 @@ import tensorflow as tf
 import keras
 from keras.optimizers import *
 from keras.callbacks import ModelCheckpoint, EarlyStopping
-from keras.applications import EfficientNetB0, EfficientNetB7
+from keras.applications import EfficientNetB0,EfficientNetB2, EfficientNetB7
 from tensorflow.python.keras import backend as K
 import sys
 import pandas as pd
 from tqdm import tqdm
-from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import threading
 import random
 import rasterio
@@ -32,6 +32,8 @@ from sklearn.model_selection import train_test_split
 import joblib
 import time
 from keras.callbacks import Callback
+import tensorflow_hub as hub
+import cv2
 
 #랜럼시드 고정
 RANDOM_STATE = 42 # seed 고정
@@ -42,7 +44,7 @@ MAX_PIXEL_VALUE = 65535 # 이미지 정규화를 위한 픽셀 최대값
 
 N_FILTERS = 16 # 필터수 지정
 N_CHANNELS = 3 # channel 지정
-EPOCHS = 100 # 훈련 epoch 지정
+EPOCHS = 120 # 훈련 epoch 지정
 BATCH_SIZE = 128 # batch size 지정
 IMAGE_SIZE = (256, 256) # 이미지 크기 지정
 MODEL_NAME = 'unet' # 모델 이름
@@ -63,7 +65,7 @@ OUTPUT_DIR = f'datasets/train_output/{save_name}/'
 WORKERS = 15
 
 # 조기종료
-EARLY_STOP_PATIENCE = 5
+EARLY_STOP_PATIENCE = 30
 
 # 중간 가중치 저장 이름
 CHECKPOINT_PERIOD = 1
@@ -91,7 +93,6 @@ experiment.set_name(f'{MODEL_NAME}_{save_name}')
 class CometLogger(Callback):
     def on_epoch_end(self, epoch, logs={}):
         experiment.log_metrics({
-            'epoch': epoch,
             'loss': logs['loss'],
             'val_loss': logs['val_loss'],
             'accuracy': logs['accuracy'],
@@ -136,6 +137,38 @@ def get_mask_arr(path):
     seg = np.float32(img)
     return seg
 
+# Data Augmentation 설정
+def get_image_data_gen():
+    #데이터가 이미지 끝부분에 걸쳐있는 경우가 많아 세밀한 조정 필요
+    data_gen_args = dict(
+        horizontal_flip=True,
+        vertical_flip=True,
+    )
+
+    image_datagen = ImageDataGenerator(**data_gen_args)
+    mask_datagen = ImageDataGenerator(**data_gen_args)
+    return image_datagen, mask_datagen
+
+#색채 대비
+def enhance_image_contrast(image):
+    # CLAHE 객체 생성
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    
+    # 이미지를 LAB 색공간으로 변환
+    lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+    l, a, b = cv2.split(lab)
+    
+    # L 채널에 CLAHE 적용
+    l_clahe = clahe.apply(l)
+    
+    # 밝기조절 - 어둡게
+    l_clahe = np.clip(l_clahe * 0.01, 0, 255).astype(l.dtype)
+    
+    # 채널 합치기 및 색공간 변환
+    enhanced_lab = cv2.merge((l_clahe, a, b))
+    enhanced_image = cv2.cvtColor(enhanced_lab, cv2.COLOR_LAB2BGR)
+    
+    return enhanced_image
 
 @threadsafe_generator
 def generator_from_lists(images_path, masks_path, batch_size=32, shuffle = True, random_state=None):
@@ -160,8 +193,13 @@ def generator_from_lists(images_path, masks_path, batch_size=32, shuffle = True,
 
         for img_path, mask_path in zip(images_path, masks_path):
 
-            img = fopen_image(img_path, bands=(7,6,8)) 
-            mask = fopen_mask(mask_path) 
+            img = fopen_image(img_path, bands=(7,6,8))
+            mask = fopen_mask(mask_path)
+            
+            #대비조절
+            img = np.uint8(img * 255)  # 이미지를 8-bit 정수 타입으로 변환
+            img = enhance_image_contrast(img)
+            img = img.astype(np.float32) / .255 #다시 32 float 타입 변환
             
             images.append(img)
             masks.append(mask)
@@ -171,6 +209,10 @@ def generator_from_lists(images_path, masks_path, batch_size=32, shuffle = True,
                 images = []
                 masks = []
 
+
+#############################################모델################################################
+
+#Default Conv2D
 def conv2d_block(input_tensor, n_filters, kernel_size = 3, batchnorm = True):
     # first layer
     x = Conv2D(filters=n_filters, kernel_size=(kernel_size, kernel_size), kernel_initializer="he_normal",
@@ -187,6 +229,7 @@ def conv2d_block(input_tensor, n_filters, kernel_size = 3, batchnorm = True):
     x = Activation("relu")(x)
     return x
 
+#UNET
 def get_unet(nClasses, input_height=256, input_width=256, n_filters = 16, dropout = 0.1, batchnorm = True, n_channels=10):
     input_img = Input(shape=(input_height,input_width, n_channels))
 
@@ -234,6 +277,7 @@ def get_unet(nClasses, input_height=256, input_width=256, n_filters = 16, dropou
     model = Model(inputs=[input_img], outputs=[outputs])
     return model
 
+#EFFI_B0
 def get_efficientunet_b0(nClasses, input_height=256, input_width=256, n_filters = 16, dropout = 0.1, batchnorm = True, n_channels=10):
     efficient_net = EfficientNetB0(weights='imagenet', include_top=False, input_shape=(input_height, input_width, n_channels), classes=nClasses)
 
@@ -272,6 +316,46 @@ def get_efficientunet_b0(nClasses, input_height=256, input_width=256, n_filters 
 
     return model
 
+#EFFI_B2
+def get_efficientunet_b2(nClasses, input_height=256, input_width=256, n_filters = 16, dropout = 0.1, batchnorm = True, n_channels=10):
+    efficient_net = EfficientNetB2(weights='imagenet', include_top=False, input_shape=(input_height, input_width, n_channels), classes=nClasses)
+
+    # Prepare feature extraction layers
+    s1 = efficient_net.get_layer('block2a_expand_activation').output
+    s2 = efficient_net.get_layer('block3a_expand_activation').output
+    s3 = efficient_net.get_layer('block4a_expand_activation').output
+    s4 = efficient_net.get_layer('block6a_expand_activation').output
+    bridge = efficient_net.get_layer('top_activation').output
+
+    # Decoding layers or expansive path
+    u1 = Conv2DTranspose(n_filters*8, (3, 3), strides=(2, 2), padding='same')(bridge)
+    u1 = concatenate([u1, s4])
+    u1 = Dropout(0.1)(u1)
+    c6 = conv2d_block(u1, n_filters=n_filters*8, kernel_size=3, batchnorm=True)
+
+    u2 = Conv2DTranspose(n_filters*4, (3, 3), strides=(2, 2), padding='same')(c6)
+    u2 = concatenate([u2, s3])
+    u2 = Dropout(0.1)(u2)
+    c7 = conv2d_block(u2, n_filters=n_filters*4, kernel_size=3, batchnorm=True)
+
+    u3 = Conv2DTranspose(n_filters*2, (3, 3), strides=(2, 2), padding='same')(c7)
+    u3 = concatenate([u3, s2])
+    u3 = Dropout(0.1)(u3)
+    c8 = conv2d_block(u3, n_filters=n_filters*2, kernel_size=3, batchnorm=True)
+
+    u4 = Conv2DTranspose(n_filters, (3, 3), strides=(2, 2), padding='same')(c8)
+    u4 = concatenate([u4, s1], axis=3)
+    u4 = Dropout(0.1)(u4)
+    c9 = conv2d_block(u4, n_filters=n_filters, kernel_size=3, batchnorm=True)
+    
+    u_final = UpSampling2D((2, 2), interpolation='bilinear')(c9)
+    outputs = Conv2D(nClasses, (1, 1), activation='sigmoid')(u_final)
+
+    model = Model(inputs=efficient_net.input, outputs=outputs)
+
+    return model
+
+#EFFI_B7
 def get_efficientunet_b7(nClasses, input_height=256, input_width=256, n_filters = 16, dropout = 0.1, batchnorm = True, n_channels=10):
     efficient_net = EfficientNetB7(weights='imagenet', include_top=False, input_shape=(input_height, input_width, n_channels), classes=nClasses)
 
@@ -310,13 +394,25 @@ def get_efficientunet_b7(nClasses, input_height=256, input_width=256, n_filters 
 
     return model
 
+#DeepLabv3+
+def get_deeplabv3plus():
+    model_url = "https://tfhub.dev/tensorflow/deeplabv3/1"
+    model = Sequential([
+        hub.KerasLayer(model_url, output_shape=[256, 256, 3], input_shape=(256, 256, 3))
+    ])
+
 def get_model(model_name, nClasses=1, input_height=128, input_width=128, n_filters = 16, dropout = 0.1, batchnorm = True, n_channels=10):
     if model_name == 'eb0':
         model = get_efficientunet_b0
+    if model_name == 'eb2':
+        model = get_efficientunet_b2
     elif model_name == 'eb7':
         model = get_efficientunet_b7
     elif model_name == 'unet':
         model =  get_unet
+    elif model == 'deeplabv3+':
+        model = get_deeplabv3plus
+        
     return model(
             nClasses      = nClasses,
             input_height  = input_height,
@@ -327,6 +423,7 @@ def get_model(model_name, nClasses=1, input_height=128, input_width=128, n_filte
             n_channels    = n_channels
         )
 
+################################### metrics ########################################
 # 두 샘플 간의 유사성 metric
 def dice_coef(y_true, y_pred, smooth=1):
     intersection = tf.reduce_sum(y_true * y_pred, axis=[1, 2, 3])
@@ -364,6 +461,8 @@ def miou(y_true, y_pred, smooth=1e-6):
     miou = tf.reduce_mean(iou)
     return miou
 
+###################################################################################
+
 #band 이미지와 마스킹 이미지 확인
 def show_band_images(image_path, mask_path):
     fig, axs = plt.subplots(3, 4, figsize=(20, 12))
@@ -396,15 +495,7 @@ def show_bands_image(image_path, band = (0,0,0)):
     return img
 
 
-########################################################################################################
-
-train_meta = pd.read_csv('datasets/train_meta.csv')
-test_meta = pd.read_csv('datasets/test_meta.csv')
-
-# 저장 폴더 없으면 생성
-if not os.path.exists(OUTPUT_DIR):
-    os.makedirs(OUTPUT_DIR)
-
+###################################################Field################################################
 
 # GPU 설정
 os.environ["CUDA_VISIBLE_DEVICES"] = str(CUDA_DEVICE)
@@ -421,14 +512,37 @@ try:
 except:
     pass
 
+train_meta = pd.read_csv('datasets/train_meta.csv')
+test_meta = pd.read_csv('datasets/test_meta.csv')
+
+# 저장 폴더 없으면 생성
+if not os.path.exists(OUTPUT_DIR):
+    os.makedirs(OUTPUT_DIR)
+    
 # for i in range(0,1000) :
 #     #데이터 확인
-#     show_band_images(IMAGES_PATH + f'train_img_{i}.tif', MASKS_PATH + f'train_mask_{i}.tif')
+    # show_band_images(IMAGES_PATH + f'train_img_{i}.tif', MASKS_PATH + f'train_mask_{i}.tif')
 #     show_bands_image(IMAGES_PATH + f'train_img_{i}.tif', (7,6,8))
+
+#대비 확인
+# fig, axs = plt.subplots(3, 4, figsize=(20, 12))
+# axs = axs.ravel()
+# for i in range(10):
+#     img = rasterio.open(IMAGES_PATH + f'train_img_{i}.tif').read([7,6,8]).transpose((1, 2, 0)).astype(np.float32) / MAX_PIXEL_VALUE
+#     img = np.uint8(img * 255)  # 이미지를 8-bit 정수 타입으로 변환
+#     img = enhance_image_contrast(img)
+#     img = img.astype(np.float32) / 255 #다시 32 float 타입 변환
+#     axs[i].imshow(img)
+#     axs[i].set_title(f'Band {i+1}')
+#     axs[i].axis('off')
+# plt.title('enhance_image_contrast')
+# plt.tight_layout()
+# plt.show() 
+
 
 # train : val = 8 : 2 나누기
 x_tr, x_val = train_test_split(train_meta, test_size=0.2, random_state=RANDOM_STATE)
-print(len(x_tr), len(x_val))
+print(len(x_tr), len(x_val)) #26860 6715
 
 # train : val 지정 및 generator
 images_train = [os.path.join(IMAGES_PATH, image) for image in x_tr['train_img'] ]
@@ -451,7 +565,7 @@ model.summary()
 # checkpoint 및 조기종료 설정
 es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=EARLY_STOP_PATIENCE, restore_best_weights=True)
 checkpoint = ModelCheckpoint(os.path.join(OUTPUT_DIR, CHECKPOINT_MODEL_NAME), monitor='val_loss', verbose=1,
-save_best_only=True, mode='auto', period=CHECKPOINT_PERIOD)
+save_best_only=True, mode='min', period=CHECKPOINT_PERIOD)
 
 
 print('---model 훈련 시작---')
@@ -467,29 +581,8 @@ history = model.fit_generator(
 )
 print('---model 훈련 종료---')
 
-"""&nbsp;
-
-## model save
-"""
 
 print('가중치 저장')
 model_weights_output = os.path.join(OUTPUT_DIR, FINAL_WEIGHTS_OUTPUT)
 model.save_weights(model_weights_output)
 print("저장된 가중치 명: {}".format(model_weights_output))
-
-#차트
-history_loss = history.history['loss']
-history_val_loss = history.history['val_loss']
-
-plt.figure(figsize=(6,6)) #세로 9 가로 6
-plt.plot(history.history['loss'], c = 'red', label = 'loss', marker = '.' )
-plt.plot(history.history['val_loss'], c = 'green', label = 'val_loss', marker = '.' )
-plt.plot(history.history['val_accuracy'], c = 'pink', label = 'val_accuracy', marker = '.' )
-plt.plot(history.history[dice_coef], c = 'brown', label = 'dice_coef', marker = '.' )
-plt.plot(history.history[pixel_accuracy], c = 'blue', label = 'pixel_accuracy', marker = '.' )
-plt.legend(loc = 'upper right')
-plt.title('')
-plt.xlabel('epoch')
-plt.ylabel('loss')
-plt.grid()
-plt.show()
